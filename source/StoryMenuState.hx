d85ce27768f2a9439aa9318b93b0778adf15a818
Picounload45 <103865615+Picounload45@users.noreply.github.com>
2022-07-03 15:09:54 +0100

    I need help

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -195,7 +195,11 @@ class StoryMenuState extends MusicBeatState
 		updateText();
 
 		trace("Line 165");
-
+    
+    #if mobileC
+    addVirtualPad(FULL, A_B);
+    #end
+    
 		super.create();
 	}
 
