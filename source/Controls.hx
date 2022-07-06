1976159cec84323e546034cbd8b00f2466f80ad8
Just a guy <103865615+just-a-guy1532@users.noreply.github.com>
2022-07-06 22:40:25 +0100

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -370,10 +370,6 @@ class Controls extends FlxActionSet
         #if android
 	public function addAndroidBack() {
 		/* fix this later
-		var BACK = #if (openfl >= "8.0.0") 0x4000010E #else 27 #end;
-		_back.addKey(BACK, JUST_RELEASED);
-		_back.addKey(BACK, JUST_PRESSED);
-		_back.addKey(BACK, PRESSED);
 	}
 	#end*/
 
